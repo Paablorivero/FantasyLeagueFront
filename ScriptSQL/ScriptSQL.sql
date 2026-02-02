532e42f53efe11e456cbb3176c2a620cbee511f7
COMPUS023\daw2 <alejandrosainzsainz@outlook.es>
2026-02-02 20:31:43 +0100

    Feature: Init SQL scripting.

---
+++ ScriptSQL/ScriptSQL.sql
@@ -0,0 +1,50 @@
+-- Database: fantasy_league
+
+DROP DATABASE IF EXISTS fantasy_league;
+
+CREATE DATABASE fantasy_league
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+--Creamos un usuario que es el que vamos a usar por ahora y le damos todos los privilegios para poder trabajar y hacer pruebas
+	create user usuario_prueba  with password 'test';
+
+	grant all privileges on fantasy_league to usuario_prueba;
+
+	drop table if exists usuarios;
+	drop table if exists equipos;
+	drop table if exists ligas;
+
+-- Empezamos a crear las tablas
+	create table if not exists usuarios (
+		usuario_id varchar(36) primary key,
+		username varchar(50) not null,
+		email varchar(100) not null,
+		f_nacim date not null
+	);
+
+
+	create table if not exists equipos(
+		equipo_id varchar(36) primary key,
+		nombre varchar(50) not null,
+		logo text,
+		usuario_id varchar(36) not null references usuarios(usuario_id)
+	);
+
+
+	create table if not exists ligas(
+		liga_id varchar(36) primary key,
+		nombre_liga varchar(50) not null,
+		participantes integer default 1,
+		usuario_id varchar(36) not null references usuarios(usuario_id)
+	);
+
+	select * from ligas;
+	
