fb9d797053afd2d1cc8d1003e9cbdef1acfc8410
COMPUS039\daw2 <servellonrejasjuandavid@gmail.com>
2026-02-06 18:38:03 +0100

    Empezando landing page

--- FLFront/src/app/app.routes.ts
+++ FLFront/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {LandinPage} from './Pages/landin-page/landin-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: "full", redirectTo: 'landingpage'},
+  {path: 'landingpage', component: LandinPage}
+  ];
