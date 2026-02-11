965ed558d3386b4365b931314ced247982bf43c6
Pablo <pablo.rivero.gutierrez@gmail.com>
2026-02-11 17:02:52 +0100

    no se que hago

--- FLFront/src/app/Pages/log-in/log-in.ts
+++ FLFront/src/app/Pages/log-in/log-in.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './log-in.html',
   styleUrl: './log-in.css',
 })
 export class LogIn {
+  passwordVisible: boolean = false;
 
+  togglePassword(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
