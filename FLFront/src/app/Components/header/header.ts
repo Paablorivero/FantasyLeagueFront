64db197aa28c9d68bd628fcda6e8ed28418ba771
DECS041\daw2 <pablo.rivero.gutierrez@gmail.com>
2026-02-24 18:24:45 +0100

    Mostrar botones u ocultarlos en funcion de pagina y token.

--- FLFront/src/app/Components/header/header.ts
+++ FLFront/src/app/Components/header/header.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +8,23 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header {
+  isLogedIn: boolean = false;
   menuOpen = signal(false);
+  router = inject(Router);
+
+  ngOnInit(): void{
+    if (localStorage.getItem('token')) {
+      this.isLogedIn = true;
+    } else {
+      this.isLogedIn = false;
+    }
+    console.log("Estado del log en el navar = ", this.isLogedIn);
+  }
+
+  mostrarBotonEntrar(): boolean {
+    const rutasOcultas = ['/daznfantasy/login', '/daznfantasy/register'];
+    return !rutasOcultas.includes(this.router.url);
+  }
 
   toggleMenu(): void {
     this.menuOpen.update(v => !v);
