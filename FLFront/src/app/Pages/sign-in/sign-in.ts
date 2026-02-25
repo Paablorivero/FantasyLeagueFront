8cc292b9ac850b20155ab0caf4505db97d74022b
Pablo Rivero <142850941+Paablorivero@users.noreply.github.com>
2026-02-25 15:49:19 +0100

    actualizacion formulario y navbar

--- FLFront/src/app/Pages/sign-in/sign-in.ts
+++ FLFront/src/app/Pages/sign-in/sign-in.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
 export class SignIn {
+  userForm: FormGroup;
  passwordVisible: boolean = false;
 
   togglePassword(): void {
